529f4be8258a325f04d8952e5ddbf23b40380ec7
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-03-03 18:15:22 +0300

    del semicolon sign

--- insertion-sort/javascript/insertion-sort.js
+++ insertion-sort/javascript/insertion-sort.js
@@ -1,78 +1,78 @@
-let firstArray = [];
-let secondArray = [1, 1, 19, 4, 5];
-let thirdArray = [-1, 2, 4, -5, 10];
-let fourthArray = [-1, -5, -12, -2, 0];
-let fifthArray =[ 37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54];
+let firstArray = []
+let secondArray = [1, 1, 19, 4, 5]
+let thirdArray = [-1, 2, 4, -5, 10]
+let fourthArray = [-1, -5, -12, -2, 0]
+let fifthArray =[ 37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54]
 
 // Generate an array with random numbers
 for(let i = 0; i < 5; i++) {
-	firstArray.push(Math.round(Math.random() * 100));
+	firstArray.push(Math.round(Math.random() * 100))
 }
 
-console.log(`Before sort 1 array: ${firstArray}\nBefore sort 2 array: ${secondArray}\nBefore sort 3 array: ${thirdArray}\nBefore sort 4 array: ${fourthArray}\nBefore binary sort 5 array: ${fifthArray}`);
+console.log(`Before sort 1 array: ${firstArray}\nBefore sort 2 array: ${secondArray}\nBefore sort 3 array: ${thirdArray}\nBefore sort 4 array: ${fourthArray}\nBefore binary sort 5 array: ${fifthArray}`)
 
 function insertionSort(arr) { 
-   let key, j; 
+   let key, j
    for (let i = 1; i < arr.length; i++) { 
-      key = arr[i]; 
-      j = i - 1; 
+      key = arr[i]
+      j = i - 1
       /* Move elements of arr[0..i-1], that are 
       greater than key, to one position ahead 
       of their current position */
       while (j >= 0 && arr[j] > key) { 
-         arr[j + 1] = arr[j]; 
-         j = j - 1; 
+         arr[j + 1] = arr[j] 
+         j = j - 1
       } 
-      arr[j + 1] = key; 
+      arr[j + 1] = key 
    } 
-	return arr;
+	return arr
 } 
 
 // binary insertion sort
 function binarySearch(a, item, low, high) {
    if (high <= low)
-      return (item > a[low]) ? (low + 1) : low;
+      return (item > a[low]) ? (low + 1) : low
   
-   mid = Math.floor((low + high) / 2);
+   mid = Math.floor((low + high) / 2)
   
    if(item == a[mid])
-      return mid + 1;
+      return mid + 1
   
    if(item > a[mid])
-      return binarySearch(a, item, mid + 1, high);
+      return binarySearch(a, item, mid + 1, high)
           
-   return binarySearch(a, item, low, mid - 1);
+   return binarySearch(a, item, low, mid - 1)
 }
  
 function binaryInsertionSort(arr) {
    for (let i = 1; i < arr.length; i++) {
-      let j = i - 1;
-      let x = arr[i];
+      let j = i - 1
+      let x = arr[i]
   
       // Find location to insert
       // using binary search
-      let loc = Math.abs(binarySearch(arr, x, 0, j));
+      let loc = Math.abs(binarySearch(arr, x, 0, j))
   
       // Shifting array to one
       // location right
              
       while (j >= loc) {
-         arr[j + 1] = arr[j];
-         j--;
+         arr[j + 1] = arr[j]
+         j--
       }
   
       // Placing element at its
       // correct location
-      arr[j+1] = x;
+      arr[j+1] = x
    }
 }
 
 // Test
-insertionSort(firstArray);
-insertionSort(secondArray);
-insertionSort(thirdArray);
-insertionSort(fourthArray);
+insertionSort(firstArray)
+insertionSort(secondArray)
+insertionSort(thirdArray)
+insertionSort(fourthArray)
 
 binaryInsertionSort(fifthArray)
 
-console.log(`After sort 1 array: ${firstArray}\nAfter sort 2 array: ${secondArray}\nAfter sort 3 array: ${thirdArray}\nAfter sort 4 array: ${fourthArray}\nAfter binary sort 5 array: ${fifthArray}`);
+console.log(`After sort 1 array: ${firstArray}\nAfter sort 2 array: ${secondArray}\nAfter sort 3 array: ${thirdArray}\nAfter sort 4 array: ${fourthArray}\nAfter binary sort 5 array: ${fifthArray}`)
--- selection-sort/javascript/selection-sort.js
+++ selection-sort/javascript/selection-sort.js
@@ -1,31 +1,31 @@
-let firstArray = [];
-let secondArray = [1, 1, 19, 4, 5];
-let thirdArray = [-1, 2, 4, -5, 10];
-let fourthArray = [-1, -5, -12, -2, 0];
-let fifthArray = [4, 5, 3, 2, 4, 1];
+let firstArray = []
+let secondArray = [1, 1, 19, 4, 5]
+let thirdArray = [-1, 2, 4, -5, 10]
+let fourthArray = [-1, -5, -12, -2, 0]
+let fifthArray = [4, 5, 3, 2, 4, 1]
 
 // Generate an array with random numbers
 for(let i = 0; i < 5; i++) {
-	firstArray.push(Math.round(Math.random() * 100));
+	firstArray.push(Math.round(Math.random() * 100))
 }
 
-console.log(`Before sort 1 array: ${firstArray}\nBefore sort 2 array: ${secondArray}\nBefore sort 3 array: ${thirdArray}\nBefore sort 4 array: ${fourthArray}\nBefore stable sort 5 array: ${fifthArray}`);
+console.log(`Before sort 1 array: ${firstArray}\nBefore sort 2 array: ${secondArray}\nBefore sort 3 array: ${thirdArray}\nBefore sort 4 array: ${fourthArray}\nBefore stable sort 5 array: ${fifthArray}`)
 
 // Simple selection sort
 function selectionSort(arr) {
 	for(let i = 0; i < arr.length - 1; i++) {
-		let indexMin = i;
+		let indexMin = i
 
 		for(let j = i + 1; j < arr.length; j++) {
 			if(arr[indexMin] > arr[j]) {
-				indexMin = j;
+				indexMin = j
 			}
 		}
 		if(indexMin !== i) {
-			[arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+			[arr[i], arr[indexMin]] = [arr[indexMin], arr[i]]
 		}
 	}
-	return arr;
+	return arr
 }
 
 // Stable selection sort
@@ -37,29 +37,29 @@ function stableSelectionSort(arr) {
    
       // Find minimum element from
       // arr[i] to arr[n - 1].
-      let min = i;
+      let min = i
       for (let j = i + 1; j < arr.length; j++) {
          if (arr[min] > arr[j]) {
-            min = j;
+            min = j
 			}
 		}
       // Move minimum element at current i.
-      let key = arr[min];
+      let key = arr[min]
       while (min > i) {
-         arr[min] = arr[min - 1];
-         min--;
+         arr[min] = arr[min - 1]
+         min--
       }
-      arr[i] = key;
+      arr[i] = key
    }
 }
 
 // Test for simple selection sort
-selectionSort(firstArray);
-selectionSort(secondArray);
-selectionSort(thirdArray);
-selectionSort(fourthArray);
+selectionSort(firstArray)
+selectionSort(secondArray)
+selectionSort(thirdArray)
+selectionSort(fourthArray)
 
 // Test for stable selection sort
-stableSelectionSort(fifthArray);
+stableSelectionSort(fifthArray)
 
-console.log(`After sort 1 array: ${firstArray}\nAfter sort 2 array: ${secondArray}\nAfter sort 3 array: ${thirdArray}\nAfter sort 4 array: ${fourthArray}\nAfter stable sort 5 array: ${fifthArray}`);
+console.log(`After sort 1 array: ${firstArray}\nAfter sort 2 array: ${secondArray}\nAfter sort 3 array: ${thirdArray}\nAfter sort 4 array: ${fourthArray}\nAfter stable sort 5 array: ${fifthArray}`)
